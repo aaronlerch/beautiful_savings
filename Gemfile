c3e03a00c93e9b4d64e8bde2b1fbbb0e8dba6c90
Aaron Lerch <aaronlerch@gmail.com>
2012-11-12 16:15:31 -0500

    Remove a bunch of stuff we don't need, and upgrade to latest gem versions

--- Gemfile
+++ Gemfile
@@ -4,7 +4,5 @@ gem 'rack'
 gem 'sinatra'
 gem 'slim'
 gem 'json'
-gem 'less'
-gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
-gem "mongoid", "~> 2.4"
-gem "bson_ext", "~> 1.5"
+gem "mongoid"
+gem "bson_ext"
--- app.rb
+++ app.rb
@@ -4,26 +4,6 @@ Bundler.require
 class App < Sinatra::Base
   set :root, File.dirname(__FILE__)
 
-  register Sinatra::AssetPack
-
-  assets {
-    serve '/js',     from: 'assets/js'
-    serve '/css',    from: 'assets/css'
-    serve '/images', from: 'assets/images'
-
-    # The second parameter defines where the compressed version will be served.
-    # (Note: that parameter is optional, AssetPack will figure it out.)
-    js :app, '/js/app.js', [
-      '/js/vendor/bootstrap.min.js',
-      '/js/vendor/**/*.js',
-      '/js/app/**/*.js'
-    ]
-
-    css :app, '/css/app.css', [
-      '/css/base.css',
-    ]
-  }
-
   configure :development do
   end
 
