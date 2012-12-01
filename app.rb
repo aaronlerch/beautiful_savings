ba5fd6a72386f384bb6b98fb32530acc293a2b58
Aaron Lerch <aaronlerch@gmail.com>
2012-11-30 23:32:41 -0500

    Add indextank gem, and implement search algorithm

--- app.rb
+++ app.rb
@@ -1,20 +1,25 @@
 require 'bundler'
 Bundler.require
+require 'shellwords'
 require 'sinatra/content_for2'
 require_relative './database.rb'
 
 class App < Sinatra::Base
   set :root, File.dirname(__FILE__)
+  set :results_page_size, 10
 
   configure do
     Database.configure :production
+    search_api ||= IndexTank::Client.new ENV['BEAUTIFULSAVINGS_INDEXDEN_URI']
+    search_index ||= search_api.indexes "beautiful_savings"
+    set :search_index, search_index
   end
 
   helpers Sinatra::ContentFor2
 
   helpers do
     def pre_process_query
-      return if params[:lat].present? and params[:lng].present?
+      return if params[:lat].to_f != 0 and params[:lng].to_f != 0
       matchdata = /\b(\d{5})\b/.match params[:q]
       return if !matchdata || !matchdata.captures.any?
       zip = matchdata.captures[0]
@@ -41,12 +46,54 @@ class App < Sinatra::Base
       coords
     end
 
-    def get_query
+    def search
       pre_process_query
-      query = params[:q]
-      tokens = query.gsub(/'/, "\\\\'").shellsplit
-      tokens.each { |token| token.downcase! }
-      #Database.companies.find({:search => { "$in" => tokens }})
+      query = build_indexden_query params[:q]
+      search_options = { 
+        #:start => page * RESULTS_PAGE_SIZE, 
+        :len => options.results_page_size, 
+        :function => 1
+      }
+
+      lat = params[:lat].to_f
+      lng = params[:lng].to_f
+      if lat != 0 && lng != 0
+        search_options[:variables] = { 0 => lat, 1 => lng }
+        search_options[:function] = 2
+      end
+
+      result = options.search_index.search(query, search_options)
+=begin
+Example return result:
+
+["matches", "184"]<br />["query", "name:a*^3 OR text:a^1"]<br />["search_time", "0.004"]<br />["results", [{"docid"=>"50b3f78dd7730010f4000025", "query_relevance_score"=>"4508"}, {"docid"=>"50b3f71bd7730010f4000010", "query_relevance_score"=>"4490"}, {"docid"=>"50b3f711d7730010f400000f", "query_relevance_score"=>"4490"}, {"docid"=>"50b3f721d7730010f4000011", "query_relevance_score"=>"4490"}, {"docid"=>"50b3f702d7730010f400000c", "query_relevance_score"=>"4489"}, {"docid"=>"50b3f791d7730010f4000026", "query_relevance_score"=>"4489"}, {"docid"=>"50b3f707d7730010f400000d", "query_relevance_score"=>"4488"}, {"docid"=>"50b3f70bd7730010f400000e", "query_relevance_score"=>"4488"}, {"docid"=>"50b3f6f9d7730010f400000a", "query_relevance_score"=>"4488"}, {"docid"=>"50b3f6fdd7730010f400000b", "query_relevance_score"=>"4488"}]]
+=end
+    end
+
+    def build_indexden_query(query)
+      return "match:all" if query.strip.empty?
+
+      # Escape single quotes. Might miss a few cases where people
+      # use single quotes instead of double, but oh well
+      list = query.gsub(/'/, "\\\\'").shellsplit
+      criteria_query = ""
+      list.each do |token|
+        criteria_query += " OR " if criteria_query.length > 0
+
+        token_prefix = token + "*"
+        token_full = token
+
+        if token =~ /\s/
+          # If the token has a string, don't do a prefix search on it, because we can't
+          # and IndexTank barfs a bit.
+          token_prefix = "\"#{token_full}\""
+          token_full = "\"#{token_full}\""
+        end
+
+        criteria_query += "name:#{token_prefix}^3 OR text:#{token_full}^1"
+      end
+
+      criteria_query
     end
   end
 
@@ -56,7 +103,7 @@ class App < Sinatra::Base
 
   get '/search' do
     redirect(to('/')) if !params[:q]
-    query = get_query
+    @companies = search
     slim :search
   end
 
